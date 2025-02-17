8e4cc11d0914b87bf384820c73705fa461045ad4
elinaviksne <elina@gmail.com>
2025-02-17 13:05:45 +0200

    installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('posts', [PostController::class, 'index'])->name('post.index');
-Route::post('posts', [PostController::class, 'store'])->name('post.store');
-Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-Route::put('posts/{post}/update', [PostController::class, 'update'])->name('post.update');
-Route::delete('posts/{post}/destroy', [PostController::class, 'destroy'])->name('post.destroy');
-Route::get('posts/create', [PostController::class, 'create'])->name('post.create');
-Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
